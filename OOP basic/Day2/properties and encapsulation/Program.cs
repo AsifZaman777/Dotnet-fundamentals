3c399581e383dfde093292c7a19ef55df554d2aa
AsifZaman777 <eramasif16@gmail.com>
2025-07-21 13:10:32 +0600

    Properties concepts added

---
+++ OOP basic/Day2/properties and encapsulation/Program.cs
@@ -0,0 +1,75 @@
+﻿class Client
+{
+    private string client_name;
+    private int client_age;
+    private string client_email;
+
+    //properties for control access to the fields
+    public string ClientName
+    {
+        get { return client_name; }
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                client_name = value;
+            }
+        }
+    }
+
+    public int ClientAge
+    {
+        get { return client_age; }
+        set
+        {
+            if (value < 0 && value >= 150)
+            {
+                throw new ArgumentException(
+                    "Please insert a valid user");
+            }
+            else
+            {
+                client_age = value;
+            }
+        }
+    }
+
+    public string ClientEmail
+    {
+        get { return client_email; }
+        set
+        {
+            if (IsValidEmailId(value))
+            {
+                client_email = value;
+            }
+            else
+            {
+                throw new ArgumentException("Please insert a valid email id");
+            }
+        }
+    }
+
+    public bool IsValidEmailId(string clientEmail)
+    {
+
+        return clientEmail.Contains("@") && clientEmail.Contains(".");
+
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+        client.ClientName = "John Doe";
+        client.ClientAge = 30;
+        client.ClientEmail = "asif@gmail.com";
+
+        Console.WriteLine($"Client Name: {client.ClientName}");
+        Console.WriteLine($"Client Age: {client.ClientAge}");
+        Console.WriteLine($"Client Email: {client.ClientEmail}");
+    }
+}
---
+++ OOP basic/Day2/properties and encapsulation/PropTypes.cs
@@ -0,0 +1,87 @@
+﻿class Account
+{
+    private string accountNumber;
+    private decimal accountBalance;
+    private string creationDate;
+
+    //types of properties
+
+    //reead-write property
+    public string AccountNumber { get; set; }
+
+    //read-only property
+    public string AccountBalance
+    {
+        get { return accountBalance.ToString(); }
+    }
+    //write-only property
+    public string CreationDate
+    {
+        set { creationDate = value; }
+    }
+    //auto-implemented property - no backing field required
+    public string AccountType { get; set; }
+
+    //computed property- no backing field required
+    public string AccountAge
+    {
+        get { DateTime creation = DateTime.Parse(creationDate); 
+              return (DateTime.Now - creation).TotalDays.ToString("F0") + " days old";
+        }
+    }
+
+    //property with validation
+    public decimal AccountBalanceValidation
+    {
+        get { return accountBalance; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Account balance cannot be negative.");
+            }
+            accountBalance = value;
+        }
+    }
+
+    //we can use private setter in the sames class
+    public decimal UpdateAccountBalance
+    {
+        set
+        {
+          if(value == accountBalance)
+            {
+                throw new ArgumentException("New balance cannot be the same as current balance.");
+            }
+          else if(value < 0)
+            {
+                throw new ArgumentException("Account balance cannot be negative.");
+            }
+          else
+            {
+                AccountBalanceValidation = value;
+            }
+    }
+
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object initializer
+            Account account = new Account
+            {
+                AccountNumber = "123456789",
+                AccountType = "Savings"
+            };
+            account.CreationDate = "2023-01-01";
+            account.UpdateAccountBalance = 1000.00m; // so now we are using udpateAccountBalance property to use the private setter
+            Console.WriteLine($"Account Number: {account.AccountNumber}");
+            Console.WriteLine($"Account Type: {account.AccountType}");
+            //Console.WriteLine($"Creation Date: {account.CreationDate}"); // we cannot access this property directly as it is write-only
+            Console.WriteLine($"Account Balance: {account.AccountBalance}");
+            Console.WriteLine($"Account Age: {account.AccountAge}");
+        }
+    }
+}
